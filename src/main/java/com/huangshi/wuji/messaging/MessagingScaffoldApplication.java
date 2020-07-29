11f7c90ac530e9502975cb76e2df658582dabd78
HuangMarco <ht576475012@163.com>
2020-07-29 15:26:12 +0800

    [new] added branch dev and rename"

---
+++ src/main/java/com/huangshi/wuji/messaging/MessagingScaffoldApplication.java
@@ -0,0 +1,13 @@
+package com.huangshi.wuji.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingScaffoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingScaffoldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huangshi/wuji/messaging/MessagingScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huangshi.wuji.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingScaffoldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
