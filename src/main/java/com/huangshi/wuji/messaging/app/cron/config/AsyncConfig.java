83dd195cfa7f0024d6da2f1e261298feec93de76
HuangMarco <ht576475012@163.com>
2020-08-16 13:52:21 +0800

    [new] added @Async

---
+++ src/main/java/com/huangshi/wuji/messaging/app/cron/config/AsyncConfig.java
@@ -0,0 +1,31 @@
+package com.huangshi.wuji.messaging.app.cron.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    /**
+     * // TODO: 2020/8/16 从配置文件中抽取value
+     */
+    private int corePoolSize = 10;
+    private int maxPoolSize = 200;
+    private int queueCapacity = 10;
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.initialize();
+        return executor;
+    }
+
+}
--- src/main/java/com/huangshi/wuji/messaging/app/cron/task/ScheduledService.java
+++ src/main/java/com/huangshi/wuji/messaging/app/cron/task/ScheduledService.java
@@ -1,11 +1,13 @@
 package com.huangshi.wuji.messaging.app.cron.task;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@Async
 public class ScheduledService {
 
     @Scheduled(cron = "0/5 * * * * *")
